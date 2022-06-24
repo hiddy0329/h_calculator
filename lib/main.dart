bed1c32aecbb79cd7bc66596e75806729dba9b8b
Hidai Satoshi <hiddy19970329@gmail.com>
2022-06-24 19:00:14 +0900

    ユーザーログインページ修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'home.dart';
 import 'mysql.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const LoginApp());
@@ -12,32 +14,32 @@ class LoginApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       // エミュレーター右上の「debug」という帯を消す
       debugShowCheckedModeBanner: false,
-      title: 'ログイン画面',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const LoginPage(title: 'Login Page'),
+      home: LoginPage(),
     );
   }
 }
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-  var _errorMessage = "";
+  // フォント設定
+  static const String font = 'Roboto';
 
-  var _nameController = TextEditingController();
-  var _passwordController = TextEditingController();
+  final _nameController = TextEditingController();
+
+  final _passwordController = TextEditingController();
+
+  final MySQL _mysql = MySQL();
+
+  bool _databaseButtonPressed = false;
 
   // 新規usernameを代入する変数
   String newUsername = "";
@@ -47,14 +49,171 @@ class _LoginPageState extends State<LoginPage> {
   String loginUsername = "";
   // ログインpasswordを代入する変数
   String loginPassword = "";
+  // ユーザーに関するエラーメッセージ
+  String _errorMessage = "";
 
-  MySQL _mysql = MySQL();
+  String currentUserId = "";
+
+  String currentUsername = "";
+
+  late SharedPreferences prefs;
+
+  Future<void> setInstance() async {
+    prefs = await SharedPreferences.getInstance();
+  }
+
+  Future<void> setUserInfo() async {
+    await prefs.setString('userId', _mysql.userList[0]);
+    await prefs.setString('username', _mysql.userList[1]);
+  }
+
+  Future<void> getUserInfo() async {
+    setState(() {
+      currentUserId = prefs.getString('userId')!;
+      currentUsername = prefs.getString('username')!;
+    });
+  }
+
+  // データベースボタンの色を変えるメソッド
+  void _changeDbButtonColor() {
+    setState(() {
+      _databaseButtonPressed = true;
+    });
+  }
+
+  // エラーメッセージ取得メソッド
+  void _getErrorMessage() {
+    setState(() {
+      if (_mysql.dbConnectExec == true) {
+        _errorMessage = "Username and Password is invalid.";
+      } else {
+        _errorMessage = "Please connect app to the database!";
+      }
+    });
+  }
+
+  // テキストボックスをクリアするメソッド
+  void _clearText() {
+    setState(() {
+      _nameController.clear();
+      _passwordController.clear();
+      loginUsername = "";
+      loginPassword = "";
+      newUsername = "";
+      newPassword = "";
+      _errorMessage = "";
+    });
+  }
+
+  void _navigateNextPage() {
+    Navigator.of(context).push(
+      PageRouteBuilder(
+        pageBuilder: (context, animation, secondaryAnimation) {
+          return MyHomePage(userId: currentUserId, username: currentUsername);
+        },
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          const Offset begin = Offset(1.0, 0.0); // 右から左
+          // final Offset begin = Offset(-1.0, 0.0); // 左から右
+          const Offset end = Offset.zero;
+          final Animatable<Offset> tween = Tween(begin: begin, end: end)
+              .chain(CurveTween(curve: Curves.easeInOut));
+          final Animation<Offset> offsetAnimation = animation.drive(tween);
+          return SlideTransition(
+            position: offsetAnimation,
+            child: child,
+          );
+        },
+      ),
+    );
+  }
+
+  // ログイン判定のためのメソッド
+  Future _checkLogin() async {
+    if (_mysql.dbConnectExec == true) {
+      await _mysql.selectFromUserDB(loginUsername, loginPassword);
+      // ログインできるかどうかの判定
+      if (_mysql.userList.contains(loginUsername) &&
+          _mysql.userList.contains(loginPassword)) {
+        // Shared_preferenceを利用してユーザーidを端末保存
+        await setUserInfo();
+        await getUserInfo();
+        _navigateNextPage();
+        setState(() {
+          _clearText();
+        });
+      } else {
+        _getErrorMessage();
+      }
+    } else {
+      _getErrorMessage();
+    }
+  }
+
+  // ユーザー登録用のメソッド
+  Future _checkRegistration() async {
+    if (_mysql.dbConnectExec == true) {
+      if (newUsername != "" && newPassword != "") {
+        await _mysql.insertUserDB(newUsername, newPassword);
+        await _mysql.selectFromUserDB(newUsername, newPassword);
+        // Shared_preferenceを利用してユーザーidを端末保存
+        await setUserInfo();
+        await getUserInfo();
+        _navigateNextPage();
+        setState(() {
+          _clearText();
+        });
+      } else {
+        _getErrorMessage();
+      }
+    } else {
+      _getErrorMessage();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setInstance();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.indigo[100],
       appBar: AppBar(
-        title: Text(widget.title),
+        backgroundColor: Colors.blueGrey[900],
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            const Text(
+              "Login Page",
+              style: TextStyle(fontFamily: font),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.transparent,
+                elevation: 0,
+              ),
+              onPressed: () {
+                _changeDbButtonColor();
+                _mysql.dbConnect();
+              },
+              child: Icon(
+                FontAwesomeIcons.database,
+                color: (_databaseButtonPressed)
+                    ? Colors.pinkAccent.shade200
+                    : Colors.white,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                (_databaseButtonPressed) ? "Database On" : "Database Off",
+                style: const TextStyle(fontFamily: font),
+              ),
+            ),
+          ],
+        ),
       ),
       body: Center(
         child: Padding(
@@ -62,12 +221,14 @@ class _LoginPageState extends State<LoginPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(_errorMessage, style: const TextStyle(color: Colors.red)),
+              Text(_errorMessage,
+                  style: const TextStyle(fontSize: 20.0, color: Colors.red)),
               // Usernameフィールド
               TextField(
                 controller: _nameController,
-                decoration: InputDecoration(
+                decoration: const InputDecoration(
                   hintText: 'Username',
+                  hintStyle: TextStyle(fontFamily: font),
                 ),
                 onChanged: (context) {
                   newUsername = context;
@@ -78,8 +239,9 @@ class _LoginPageState extends State<LoginPage> {
               // Passwordフィールド
               TextField(
                 controller: _passwordController,
-                decoration: InputDecoration(
+                decoration: const InputDecoration(
                   hintText: 'Password',
+                  hintStyle: TextStyle(fontFamily: font),
                 ),
                 onChanged: (context) {
                   newPassword = context;
@@ -89,38 +251,42 @@ class _LoginPageState extends State<LoginPage> {
               ButtonBar(
                 children: <Widget>[
                   TextButton(
-                    child: const Text('CANCEL'),
+                    style: TextButton.styleFrom(primary: Colors.blueGrey[900]),
                     onPressed: () {
-                      _nameController.clear();
-                      _passwordController.clear();
+                      _clearText();
                     },
+                    child: const Text(
+                      'Cancel',
+                      style: TextStyle(fontFamily: font),
+                    ),
                   ),
                   ElevatedButton(
-                      child: const Text('LOGIN'),
-                      onPressed: () {
-                        // TODO ユーザーログイン時にデータベースに登録されているユーザー情報かどうか判定する
-                        // if (_mysql.userList.contains(loginUsername) &&
-                        //     _mysql.userList.contains(loginPassword)) {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  //履歴ページへ値を送る
-                                  builder: (context) => MyApp()));
-                        }
-                      // }
-                      ),
+                    style: ElevatedButton.styleFrom(
+                      side: BorderSide.none,
+                      primary: Colors.blueGrey[900],
+                    ),
+                    onPressed: () {
+                      // _mysql.dbConnect();
+                      _checkLogin();
+                    },
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(fontFamily: font),
+                    ),
+                  ),
                   ElevatedButton(
-                      child: const Text('SIGNUP'),
-                      onPressed: () {
-                        _mysql.main();
-                        _mysql.manipulateUserDB(newUsername, newPassword);
-                        //サインアップ処理
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                //履歴ページへ値を送る
-                                builder: (context) => MyApp()));
-                      }),
+                    style: ElevatedButton.styleFrom(
+                      side: BorderSide.none,
+                      primary: Colors.blueGrey[900],
+                    ),
+                    onPressed: () {
+                      _checkRegistration();
+                    },
+                    child: const Text(
+                      'Sign Up',
+                      style: TextStyle(fontFamily: font),
+                    ),
+                  ),
                 ],
               ),
             ],
