d40f237d5537ac621a56c98d723efd8d0bb463b1
Hidai Satoshi <hiddy19970329@gmail.com>
2022-07-06 22:27:08 +0900

    homeファイルを変更

--- lib/home.dart
+++ lib/home.dart
@@ -1,12 +1,9 @@
 // ignore_for_file: prefer_const_constructors
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'logic.dart';
 import 'mysql.dart';
 import 'history_page.dart';
-import 'dart:math' as math;
-import 'package:intl/intl.dart' as intl;
 
 class MyHomePage extends StatefulWidget {
   final String userId;
@@ -32,320 +29,66 @@ class _MyHomePageState extends State<MyHomePage> {
   // 画面上に表示する内容を格納する変数
   String text = "";
 
-  // 値を表示する変数
-  double displayedNumber = 0;
-
-  // 現在値を格納する変数
-  double _setCurrentNumber = 0;
-
-  // 画面に出力できる最大値
-  static const MAX_DEGIT = 9;
-
-  //掛け算・割り算の結果を保持する変数
-  double _memorialValue = 0;
-
-  //足し算・引き算の結果を保持する変数
-  double _previousValue = 0;
-
-  //掛け算・割り算の演算子を記録しておく変数
-  String _previousOperation = "";
-
-  //足し算・引き算の演算子を記録しておく変数
-  String _memorialOperation = "";
-
-  // 小数点ボタンが押されたかどうかを示すbool値
-  bool _decimalFlag = false;
-
-  // "."が押された後の数値の数をカウントする変数
-  int _numAfterPoint = 0;
-
-  //桁区切り実装用
-  intl.NumberFormat formatter = intl.NumberFormat('#,###.########', 'en_US');
-
-  // 画面上部に出力するメッセージ
-  String _cheeringMessage = "";
-
-  // String型に変換したdisplayedNumber
-  String displayedNumberAsString = "";
-
-  // データベースに保存する計算式の部分を格納するリスト
-  String formula = "";
+  String txtResult = "0";
 
   final MySQL _mysql = MySQL(); // MySQLクラスのインスタンスを作成
 
-  //入力値をセットするメソッド
-  void input(String text) {
-    setState(() {
-      if (text == ".") {
-      _decimalFlag = true;
-      formula += '${text}';
-    } else {
-      int degit = getDegit(_setCurrentNumber);
+  final Logic _logic = Logic(); // Logicクラスのインスタンス作成
 
-      // 整数部分と少数部分を合わせて表示桁数が9桁以上は表示させない
-      if (degit + _numAfterPoint == MAX_DEGIT) {
-        //処理なし
-      } else if (_decimalFlag) {
-        _numAfterPoint++;
-        if (displayedNumber >= 0) {
-          _setCurrentNumber =
-            _setCurrentNumber + int.parse(text) * math.pow(0.1, _numAfterPoint);
-        } else {
-          _setCurrentNumber =
-          _setCurrentNumber - int.parse(text) * math.pow(0.1, _numAfterPoint);
-        }
-        // 整数を入力した時、初期状態だった時
-      } else if (_setCurrentNumber == 0) {
-        _setCurrentNumber = double.parse(text);
-        // 連続入力対応
-      } else {
-        if (displayedNumber > 0) {
-          _setCurrentNumber = _setCurrentNumber * 10 + double.parse(text);
-        } else {
-          _setCurrentNumber = _setCurrentNumber * 10 - double.parse(text);
-        }
-        
-      }
-      //最終的にgetDisplayTextメソッドに送る数値を決定
-      displayedNumber = _setCurrentNumber;
-      _cheeringMessage = "";
-      formula += '${text}';
-    }
-    });
-    
+  @override
+  void initState() {
+    super.initState();
+    _mysql.dbConnect();
   }
 
-  //画面表示用テキスト作成メソッド(小数点以下がない時は-1を取得)
-  String getDisplayText(double value, {int numAfterPoint = -1}) {
-    // 少数の時
-    if (numAfterPoint != -1) {
-      int intPart = value.toInt();
-      // 初めて"."が押された時
-      if (_decimalFlag == false && text.contains(".")) {
-        return formatter.format(value);
-      }
-      else if (numAfterPoint == 0) {
-        return formatter.format(value) + ".";
-        // "1.003などへの対応
-      } else if (intPart == value) {
-        //文字列の足し算のため、「333」+「0.0」は「3330.0」となってしまうのを回避する
-        return formatter.format(intPart) +
-            (value - intPart).toStringAsFixed(numAfterPoint).substring(1);
-      }
-    }
-    // 単なる整数の時
-    return formatter.format(value);
-  }
-
-  //桁数を取得するメソッド
-  int getDegit(double value) {
-    int i = 0;
-    if (value > 0) {
-      for (; 10 <= value; i++) {
-      value = value / 10;
-      }
-    } else {
-      for (; value <= -10; i++) {
-      value = value / 10;
-    }
-    }
-    return i + 1;
-  }
-
-  // 数値の符号を切り替えるメソッド
-  void _invertNum() {
-    setState(() {
-      displayedNumber = -displayedNumber;
-      _setCurrentNumber = -_setCurrentNumber;
-    });
-  }
-
-  // 一の位の数値を削除していくメソッド
-  void _deleteOnesPlace() {
-    setState(() {
-      String displayedNumberAsString = displayedNumber.toString();
-      // double型を文字列に変えたため、整数も小数もデフォルトで文字数が「3」になる
-      if (displayedNumberAsString.length > 3) {
-        // 単なる整数値の時（例：24.0)
-        if (displayedNumberAsString[displayedNumberAsString.length - 1] ==
-            "0") {
-          displayedNumberAsString = displayedNumberAsString.substring(
-              0, displayedNumberAsString.length - 3);
-        } else {
-          displayedNumberAsString = displayedNumberAsString.substring(
-              0, displayedNumberAsString.length - 1);
-        }
-        // 小数点数で、「.000~」となるときは、double型に変換すると一気に「0.0」まで戻ってしまう
-        if (displayedNumberAsString != "-") {
-          displayedNumber = double.parse(displayedNumberAsString);
-        }
-        _numAfterPoint--;
-        _decimalFlag = false;
-      }
-    });
-  }
-
-  // 画面上の数値をオールクリアするメソッド
-  void _clearNum() {
-    setState(() {
-      displayedNumber = 0;
-      _setCurrentNumber = 0;
-      _previousValue = 0;
-      _memorialValue = 0;
-      _previousOperation = "";
-      _memorialOperation = "";
-      // _operatorTypeも初期化したい
-      _decimalFlag = false;
-      _cheeringMessage = "All Clear!";
-      _numAfterPoint = 0;
-    });
-  }
-
-  // 計算結果に応じて表示するメッセージを切り替えるメソッド
-  void _getCheeringMessage() {
-    setState(() {
-      if (_previousOperation == "×") {
-      _cheeringMessage = "Excellent!";
-    } else if (displayedNumber == double.infinity || displayedNumber.toString() == 'NaN') {
-      _cheeringMessage = "Sorry, but I have no idea...";
-    } else if (_previousOperation == "÷") {
-      _cheeringMessage = "Perfect!";
-    } else if (_memorialOperation == "+") {
-      _cheeringMessage = "Nice Job!";
-    } else {
-      _cheeringMessage = "Awesome!";
-    } 
-    });
-  }
-
-  // 途中計算を行うメソッド
-  void _halfwayCalculation(String operatorType) {
-    setState(() {
-      if (operatorType == "×" || operatorType == "÷") {
-      if (_previousOperation == "") {
-        _previousValue = _setCurrentNumber;
-      } else if (_previousOperation == "×") {
-        //直前にセットされた値と新しく入力された値を掛ける
-        _previousValue = _previousValue * _setCurrentNumber;
-      } else {
-        _previousValue = _previousValue / _setCurrentNumber;
-      }
-      displayedNumber = _previousValue;
-      _setCurrentNumber = 0;
-      _numAfterPoint = 0;
-      _decimalFlag = false;
-      _previousOperation = operatorType;
-      formula += ' ${_previousOperation} ';
-      // (1 × 4 + 2 × 4 + ...)などの掛け算の結果を足し合わせていく場合に対応
-    } else if (operatorType == "+" || operatorType == "-") {
-      if (_previousOperation == "×") {
-        if (operatorType == "+" && _memorialOperation == "-") {
-           _memorialValue = (_memorialValue - (_previousValue * _setCurrentNumber)).abs();
-        } else if (operatorType == "+") {
-          _memorialValue += (_previousValue * _setCurrentNumber);
-        } else if (operatorType == "-" && _memorialOperation == "+") {
-          _memorialValue += (_previousValue * _setCurrentNumber);
-        } else {
-          _memorialValue = (_memorialValue - (_previousValue * _setCurrentNumber)).abs();
-        }
-        _previousValue = 0;
-        _previousOperation = "";
-      } else if (_previousOperation == "÷") {
-        if (operatorType == "+" && _memorialOperation == "-") {
-          _memorialValue = (_memorialValue - (_previousValue / _setCurrentNumber)).abs();
-        } else if (operatorType == "+") {
-          _memorialValue += (_previousValue / _setCurrentNumber);
-        } else if (operatorType == "-" && _memorialOperation == "+") {
-          _memorialValue += (_previousValue / _setCurrentNumber);
-        } else {
-          _memorialValue = (_memorialValue - (_previousValue / _setCurrentNumber)).abs();
-        }
-        _previousValue = 0;
-        _previousOperation = "";
-      } else if (_memorialOperation == "") {
-        _memorialValue = _setCurrentNumber;
-      } else if (_memorialOperation == "+") {
-        _memorialValue = _memorialValue + _setCurrentNumber;
-      } else if (_memorialOperation == "-") {
-        _memorialValue = _memorialValue - _setCurrentNumber;
-      }
-
-      displayedNumber = _memorialValue;
-      _setCurrentNumber = 0;
-      _numAfterPoint = 0;
-      _decimalFlag = false;
-      _memorialOperation = operatorType;
-      formula += ' ${_memorialOperation} ';
-    }
-    });
-  }
-
-  // 最終的な計算結果を求めるメソッド
-  void _finalCalculation() {
-    setState(() {
-      if (_previousOperation == "×" || _previousOperation == "÷") {
-        double result = (_previousOperation == "×")
-            ? _previousValue * _setCurrentNumber
-            : _previousValue / _setCurrentNumber;
-        
-        displayedNumber = (_memorialOperation == "-")
-            ? _memorialValue - result
-            : _memorialValue + result;
-      } else if (_memorialOperation == "+") {
-        displayedNumber = _memorialValue + _setCurrentNumber;
-      } else {
-        displayedNumber = _memorialValue - _setCurrentNumber;
-      }
-      _getCheeringMessage();
-      _setCurrentNumber = displayedNumber;
-      _previousValue = 0;
-      _memorialValue = 0;
-      _numAfterPoint = 0;
-      _decimalFlag = false;
-      formula += ' =';
-    });
-  }
-  
   // ボタンをウィジェット化
   Widget button(String text, Color colorButton, Color colorText) {
     return SizedBox(
       width: text == "0" ? 195 : 94,
-      height: 87,
+      height: 85,
       child: Padding(
         padding: const EdgeInsets.all(1.0),
         child: ElevatedButton(
-          onPressed: () async{
-            switch (text) {
-              case "AC":
-                _clearNum();
-                break;
-              case "+/-":
-                _invertNum();
-                break;
-              case "Del":
-                _deleteOnesPlace();
-                break;
-              case "÷":
-                _halfwayCalculation("÷");
-                break;
-              case "×":
-                _halfwayCalculation("×");
-                break;
-              case "-":
-                _halfwayCalculation("-");
-                break;
-              case "+":
-                _halfwayCalculation("+");
-                break;
-              case "=":
-                _finalCalculation();
-                await _mysql.manipulateCalcDB(formula, displayedNumber, widget.userId);
-                formula = "";
-                break;
-              default:
-                input(text);
-                break;
-            }
+          onPressed: () {
+            setState(() {
+              switch (text) {
+                case "AC":
+                  _logic.clearNum(text);
+                  break;
+                case "+/-":
+                  _logic.invertNum(text);
+                  break;
+                case "Del":
+                  _logic.deleteOnesPlace();
+                  break;
+                case "÷":
+                  _logic.halfwayCalculation(text);
+                  break;
+                case "×":
+                  _logic.halfwayCalculation(text);
+                  break;
+                case "-":
+                  _logic.halfwayCalculation(text);
+                  break;
+                case "+":
+                  _logic.halfwayCalculation(text);
+                  break;
+                case "=":
+                  _logic.finalCalculation();
+                  try {
+                    _mysql.manipulateCalcDB(
+                        _logic.formula, _logic.displayedNumber, widget.userId);
+                  } catch (e) {
+                    txtResult = e.toString();
+                  }
+                  _logic.formula = "";
+                  break;
+                default:
+                  _logic.input(text);
+                  break;
+              }
+              txtResult = _logic.text;
+            });
           },
           style: ElevatedButton.styleFrom(
             primary: colorButton,
@@ -385,76 +128,89 @@ class _MyHomePageState extends State<MyHomePage> {
           // 計算履歴表示ボタンエリア
           appBar: AppBar(
             backgroundColor: colorNum,
-            toolbarHeight: 75.0,
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "${widget.username.toUpperCase()}'s calculator",
-                      style: TextStyle(color: colorFunc),
-                    ),
-                    TextButton(
-                      child: Text("<< logout",
-                          style: TextStyle(
-                              fontFamily: font, fontSize: 20, color: colorText)),
-                      onPressed: () {
-                        Navigator.pop(context, widget.userId);
-                      },
-                    ),
-                  ],
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.transparent,
-                    elevation: 0,
+            toolbarHeight: 77.0,
+            title: FittedBox(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      Text(
+                        "${widget.username.toUpperCase()}'s calculator",
+                        style: TextStyle(
+                            fontFamily: font,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18.0,
+                            color: colorFunc),
+                      ),
+                      TextButton(
+                        child: Text("<< logout",
+                            style: TextStyle(
+                                fontFamily: font,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18.0,
+                                color: colorText)),
+                        onPressed: () {
+                          Navigator.pop(context, widget.userId);
+                        },
+                      ),
+                    ],
                   ),
-                  onPressed: () {},
-                  child: Icon(
-                    FontAwesomeIcons.database,
-                    color: colorFunc,
-                  ),
-                ),
-                Text(
-                  "database on",
-                  style: TextStyle(
-                    fontFamily: font,
-                  ),
-                ),
-                ElevatedButton(
+                  ElevatedButton(
                     style: ElevatedButton.styleFrom(
                       primary: Colors.transparent,
                       elevation: 0,
                     ),
-                    onPressed: () {
-                      Navigator.of(context).push(
-                        PageRouteBuilder(
-                          pageBuilder:
-                              (context, animation, secondaryAnimation) {
-                            return HistoryPage(_mysql.formulaLists, _mysql.resultLists, widget.username);
-                          },
-                          transitionsBuilder:
-                              (context, animation, secondaryAnimation, child) {
-                            final Offset begin = Offset(1.0, 0.0); // 右から左
-                            // final Offset begin = Offset(-1.0, 0.0); // 左から右
-                            const Offset end = Offset.zero;
-                            final Animatable<Offset> tween =
-                                Tween(begin: begin, end: end)
-                                    .chain(CurveTween(curve: Curves.easeInOut));
-                            final Animation<Offset> offsetAnimation =
-                                animation.drive(tween);
-                            return SlideTransition(
-                              position: offsetAnimation,
-                              child: child,
-                            );
-                          },
-                        ),
-                      );
-                    },
-                    child: Icon(FontAwesomeIcons.clockRotateLeft)),
-              ],
+                    onPressed: () {},
+                    child: Icon(
+                      FontAwesomeIcons.database,
+                      color: colorFunc,
+                    ),
+                  ),
+                  Text(
+                    "database on",
+                    style: TextStyle(
+                      fontFamily: font,
+                    ),
+                  ),
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.transparent,
+                        elevation: 0,
+                      ),
+                      onPressed: () async {
+                        if (_mysql.dbConnectExec == false) {
+                          await _mysql.dbConnect();
+                        }
+                        await _mysql.selectFromCalcDB(widget.userId);
+                        Navigator.of(context).push(
+                          PageRouteBuilder(
+                            pageBuilder:
+                                (context, animation, secondaryAnimation) {
+                              return HistoryPage(_mysql.formulaLists,
+                                  _mysql.resultLists, widget.username);
+                            },
+                            transitionsBuilder: (context, animation,
+                                secondaryAnimation, child) {
+                              final Offset begin = Offset(1.0, 0.0); // 右から左
+                              // final Offset begin = Offset(-1.0, 0.0); // 左から右
+                              const Offset end = Offset.zero;
+                              final Animatable<Offset> tween = Tween(
+                                      begin: begin, end: end)
+                                  .chain(CurveTween(curve: Curves.easeInOut));
+                              final Animation<Offset> offsetAnimation =
+                                  animation.drive(tween);
+                              return SlideTransition(
+                                position: offsetAnimation,
+                                child: child,
+                              );
+                            },
+                          ),
+                        );
+                      },
+                      child: Icon(FontAwesomeIcons.clockRotateLeft)),
+                ],
+              ),
             ),
           ),
           body: Column(
@@ -464,7 +220,7 @@ class _MyHomePageState extends State<MyHomePage> {
               Expanded(
                 flex: 1,
                 child: Text(
-                  _cheeringMessage,
+                  _logic.cheeringMessage,
                   style: TextStyle(
                       fontFamily: font,
                       fontSize: 34.0,
@@ -476,10 +232,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 flex: 1,
                 child: FittedBox(
                   child: Text(
-                    (_decimalFlag) ?
-      text = getDisplayText(displayedNumber, numAfterPoint: _numAfterPoint)
-  : text = getDisplayText(displayedNumber),
-    
+                    txtResult,
                     style: TextStyle(
                         fontFamily: font, fontSize: 75.0, color: colorNum),
                   ),
